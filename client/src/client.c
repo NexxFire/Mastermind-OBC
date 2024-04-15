16bd2fc4a163649a77c1dc960a183899d2e222f4
NexxFire <martin.ducoulombier@free.fr>
2024-04-15 14:16:22 +0200

    Add color for client interface

--- client/src/client.c
+++ client/src/client.c
@@ -1,12 +1,14 @@
 #include "client.h" // client/include/client.h
 
 int main() {
+    //game structure
     game_t game;
     initGame(&game);
     showGame(game);
     
     return 0;
 }
+
 
 
 
--- client/src/show.c
+++ client/src/show.c
@@ -4,6 +4,7 @@
 void showGame(game_t game) {
     for (int i = 0; i < MAX_ROUND; i++) {
         for (int j = 0; j < BOARD_WIDTH; j++) {
+            
             printf("%c ", game.board[i][j]);
         }
         printf("\n");
@@ -22,5 +23,29 @@ void showGame(game_t game) {
         printf("Good place: %d\n", game.otherPlayers[i].nbGoodPlace);
         printf("Good color: %d\n", game.otherPlayers[i].nbGoodColor);
         printf("\n");
+    }
+}
+
+void showChar(char c) {
+    if (c == 'R') {
+        printf(ANSI_BACKGROUND_RED"  "ANSI_RESET_ALL);
+    }
+    if (c == 'G') {
+        printf(ANSI_BACKGROUND_GREEN"  "ANSI_RESET_ALL);
+    }
+    if (c == 'B') {
+        printf(ANSI_BACKGROUND_BLUE"  "ANSI_RESET_ALL);
+    }
+    if (c == 'Y') {
+        printf(ANSI_BACKGROUND_YELLOW"  "ANSI_RESET_ALL);
+    }
+    if (c == 'C') {
+        printf(ANSI_BACKGROUND_CYAN"  "ANSI_RESET_ALL);
+    }
+    if (c == 'M') {
+        printf(ANSI_BACKGROUND_MAGENTA"  "ANSI_RESET_ALL);
+    }
+    if (c == EMPTY) {
+        printf("  ");
     }
 }
